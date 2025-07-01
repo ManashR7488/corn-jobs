eb2eefd58b4a0c7ad7685b2b21b9763594a18d1b
ManashR <mahanandmanash91@gmail.com>
2025-07-01 13:59:36 +0530

    fix previous url issues

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import axios from "axios";
 import { api } from "./const.js";
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const app = express();
 const port = 5000;
@@ -9,36 +12,41 @@ app.get("/", (req, res) => {
   console.log("This APP");
 });
 
-const localUrl = process.env = "production"  ? "https://corn-jobs.onrender.com" : "http://localhost:5000"
+const localUrl = (process.env.NODE_ENV == "production"
+  ? "https://corn-jobs.onrender.com"
+  : "http://localhost:5000");
 
 async function get() {
   try {
-        const responce = await axios.get("/")
-        console.log(responce)   
-    } catch (error) {
-        console.log("error in this APP","ERROR Is:",error)
-    }
+    const responce = await axios.get(localUrl);
+    console.log(responce);
+  } catch (error) {
+    console.log("error in this APP", "ERROR Is:", error);
+  }
 }
 
 async function cornJob() {
-  api.forEach(async link =>{
+  api.forEach(async (link) => {
     try {
-        const responce = await axios.get(link)
-        console.log(responce)   
+      const responce = await axios.get(link);
+      console.log(responce);
     } catch (error) {
-        console.log("error in this link:",link ,"\n","ERROR Is:",error)
+      console.log("error in this link:", link, "\n", "ERROR Is:", error);
     }
-  })
+  });
 }
 
-
-
-app.listen(port, () => {
-  console.log(`connected to port: ${port}`);
+const StartListening = () => {
   setInterval(() => {
     get();
   }, 2000);
   setInterval(() => {
     cornJob();
   }, 4000);
+};
+
+app.listen(port, () => {
+  console.log(`connected to port: ${port}`);
+  console.log(localUrl)
+  StartListening();
 });
